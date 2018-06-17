086d6e4b1d7c37a9ae94b67dd2d126ae6bb0f169
Ruslan Kuchumov <kuchumovri@gmail.com>
2018-06-17 15:31:43 +0300

    fix ycm

--- tests/.ycm_extra_conf.py
+++ tests/.ycm_extra_conf.py
@@ -10,9 +10,7 @@ flags = [
     '-I', 'include',
     '-I', 'build',
     '-I', '/usr/src/gtest',
-    '-I', '/usr/src/gtest/include',
-    '-I', '/usr/include/glib-2.0', 
-    '-I', '/usr/lib/x86_64-linux-gnu/glib-2.0/include'
+    '-I', '/usr/src/gtest/include'
 ]
 
 SOURCE_EXTENSIONS = [ '.cc', ]
