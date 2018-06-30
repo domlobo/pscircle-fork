8387b6d12bdc8777aa33266e0d6f3c42296aebcb
Ruslan Kuchumov <kuchumovri@gmail.com>
2018-06-30 14:14:00 +0300

    Rename examples, add examples output to docs

--- examples/04-bottom.sh
+++ examples/04-bottom.sh
@@ -3,7 +3,6 @@
 set -e
 
 pscircle \
-	--output=/home/rk/pscircle.png \
 	--background-color=fff \
 	--link-color-min=aaa \
 	--link-color-max=900 \
--- examples/no-toplists-headers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pscircle \
-	--cpulist-show-header=false \
-	--memlist-show-header=false
