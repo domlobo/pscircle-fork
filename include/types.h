19cbad0f9f27c86ba802dc7bc0561479999ce0ac
Ruslan Kuchumov <kuchumovri@gmail.com>
2018-08-19 13:37:13 +0300

    ignore assert if not defined

--- include/types.h
+++ include/types.h
@@ -16,3 +16,9 @@ typedef double real_t;
 typedef PSC_NODE_COUNT_TYPE nnodes_t;
 
 typedef PSC_MEMORY_UNIT_TYPE memunit_t;
+
+// Some users report assert failures,
+// althought NDEBUG is set in release mode
+#ifndef assert
+#define assert(expr) (void (0))
+#endif
